a93cf92e84d1bff849e8983de7446cc5eefd552e
nahid-hasan <mhnahid1w3r@gmail.com>
2025-11-19 11:16:11 +0600

    added live link and screenshot section

---
+++ .history/src/main_20251010223322.jsx
@@ -0,0 +1,56 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Root from './component/Root.jsx';
+import Home from './component/Home.jsx';
+import ErrorPage from './component/ErrorPage.jsx';
+import AppsPage from './component/AppsPage.jsx';
+import AppDetails from './component/AppDetails.jsx';
+import InstallPage from './component/InstallPage.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root></Root>,
+    errorElement: <ErrorPage></ErrorPage>,
+    children: [
+      {
+        index: true,
+        path: "/",
+        loader: () => fetch("/apps.json"),
+        element: <Home></Home>
+      },
+      {
+        path: "/apps",
+        loader: () => fetch("/apps2.json"),
+        element: <AppsPage></AppsPage>
+      },
+      {
+        path: "/appdetails/:id",
+        loader: async () => {
+          const res1 = await fetch("/apps.json");
+          const data1 = await res1.json();
+          const res2 = await fetch("/apps2.json");
+          const data2 = await res2.json();
+          return [...data1, ...data2];
+        },
+        element: <AppDetails></AppDetails>
+      },
+      {
+        path: "/install",
+        element: <InstallPage></InstallPage>
+      }
+    ]
+  },
+]);
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+)
